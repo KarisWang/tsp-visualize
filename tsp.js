7b3f44e956085ca15028ebbbd172a1025ea3dd45
Karis Wang <karisrwang@gmail.com>
2020-12-04 23:05:47 -0500

    draw path function

--- tsp.js
+++ tsp.js
@@ -1,3 +1,5 @@
+const delay = ms => new Promise(res => setTimeout(res, ms))
+
 let x_list = []
 let y_list = []
 
@@ -18,7 +20,10 @@ export function addNewCoord(x, y) {
     return false
 }
 
-export function two_opt() {
+export const two_opt = async () => {
+    var canvas = document.querySelector('canvas'),
+    context = canvas.getContext('2d');
+    var imageData = context.getImageData(0,0,canvas.width,canvas.height);
     let distance_matrix = []
     for(let i = 0; i < x_list.length; i++) {
         distance_matrix[i] = []
@@ -36,6 +41,8 @@ export function two_opt() {
     let i = 0
     while (no_swap < x_list.length) {
         console.log(current_path)
+        context.putImageData(imageData, 0, 0);
+        draw_path(current_path)
         let previous_length = distance_matrix[current_path[i]][current_path[(i+1)%current_path.length]] + 
                                 distance_matrix[current_path[(i+1)%current_path.length]][current_path[(i+2)%current_path.length]] + 
                                 distance_matrix[current_path[(i+2)%current_path.length]][current_path[(i+3)%current_path.length]]
@@ -45,6 +52,7 @@ export function two_opt() {
         console.log(previous_length)
         console.log(swap_length)
         if (swap_length < previous_length) {
+            await delay(500)
             let temp = current_path[(i+1)%current_path.length]
             current_path[(i+1)%current_path.length] = current_path[(i+2)%current_path.length]
             current_path[(i+2)%current_path.length] = temp
@@ -59,4 +67,26 @@ export function two_opt() {
         console.log(current_path)
     }
     console.log(current_path)
+}
+
+function draw_path(current_path) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    for (let i = 0; i < current_path.length; i++) {
+        let point = current_path[i]
+        let nextPoint = current_path[(i+1)%current_path.length]
+
+        let xPoint = x_list[point]
+        let yPoint = y_list[point]
+
+        let xNext = x_list[nextPoint]
+        let yNext = y_list[nextPoint]
+
+        ctx.beginPath();
+        ctx.moveTo(xPoint*10+4.5, yPoint*10+4.5);
+        ctx.lineTo(xNext*10+4.5, yNext*10+4.5)
+        ctx.strokeStyle = "red"
+        ctx.stroke();
+    }
+
 }
